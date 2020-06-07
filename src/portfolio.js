8a7bf52bf1c2d7ecc3eedc678aae60fe6fd431bd
Awais Ahmad <awaisa302@gmail.com>
2020-06-08 01:35:10 +0500

    initial push

---
+++ src/portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function Name(props)
+{
+    return (
+    <div> My name is {props.name}
+    <br/>
+    <img src="/images/awais.jpg" alt="awais ahmad image"></img>
+    <h1>Assignment by Awais Ahmad
+       <b> Batch 35 </b>
+    </h1>
+     </div>
+        
+     
+    )
+}
+
+export default Name;
