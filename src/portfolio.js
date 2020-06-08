18018425e8f58068128f53ac5208bf7f8c96375b
Awais Ahmad <awaisa302@gmail.com>
2020-06-09 01:36:48 +0500

    change image word from image tag

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,7 +6,7 @@ function Name(props)
     return (
     <div> My name is {props.name}
     <br/>
-    <img src="/images/awais.jpg" alt="awais ahmad image"/>
+    <img src="/images/awais.jpg" alt="awais ahmad"/>
     <h1>Assignment by Awais Ahmad
        <b> Batch 35 </b>
     </h1>
